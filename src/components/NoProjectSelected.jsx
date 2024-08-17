8a677a36498fa5d12c3092a0bee3070bf28e9442
Andrew Chan <andrewhongchan@gmail.com>
2024-08-17 10:43:05 -0400

    Add 'NoProjectSelected' component markup

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,10 @@
+export default function NoProjectSelected() {
+    return <div>
+        <img src="" alt="" />
+        <h2>No Project Selected</h2>
+        <p>Selected a project or get started with a new one</p>
+        <p>
+            <button>Create new project</button>
+        </p>
+    </div>
+}
