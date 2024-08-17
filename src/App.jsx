77bcd38c7a24f56776204753338f4a9fb91fffd8
Andrew Chan <andrewhongchan@gmail.com>
2024-08-17 10:59:40 -0400

    Add project state logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined, // undefined: doing nothing, null: adding new project
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
   return (
     <main className="h-screen my-8 flex gap-8 ">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
