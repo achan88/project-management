e704a862dfb3b4f8780f516c1388a39b13400d95
Andrew Chan <andrewhongchan@gmail.com>
2024-08-17 10:36:21 -0400

    More styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 export default function Input({ label, textarea, ...props}) {
     return <p>
-        <label></label>
+        <label>{label}</label>
         {textarea ? <textarea {...props}></textarea> : <input {...props}/>}
 
     </p>
